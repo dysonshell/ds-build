32fcbce455249f6e3cb9110f475a9c4018968bde
Anson Yu Wang <opensource@undozen.com>
2015-01-19 20:26:18 +0800

    copy from ds's gulpfile.js

--- index.js
+++ index.js
@@ -1,144 +1,249 @@
 'use strict';
-var $ = require('gulp-load-plugins')();
+var fs = require('fs');
 var path = require('path');
-var xtend = require('xtend');
-var mime = require('mime');
+var $ = require('gulp-load-plugins')();
+var bpack = require('browserify/node_modules/browser-pack');
+var dsAssets = require('@ds/assets');
+var assign = require('lodash-node/modern/objects/assign');
 var through = require('through2');
-var assets = require('@ds/assets');
-var rewrite = require('rev-rewriter');
+var es = require('event-stream');
+var streamCombine = require('stream-combiner');
+var rewriter = require('@ds/rewriter');
+var exec = require('child_process')
+    .exec;
+var spawn = require('child_process')
+    .spawn;
 
-module.exports = function (gulp, app, rootPath) {
-    function src(glob, opts) {
-        var xopts = {
-            cwd: rootPath
-        };
-        opts = opts ? xtend(xopts, opts) : xopts;
-        return gulp.src.call(gulp, glob, opts);
-    }
+module.exports = function (gulp, config, watchify, appRoot) {
 
-    var assetsCache = {};
-    var assetsRevMap = {};
-    var port = process.env.PORT;
+var port = parseInt(process.env.PORT, 10) || config.port;
 
-    function processAssets(stream) {
-        return stream
-            .pipe(through.obj(function (file, enc, cb) {
-                file.base = rootPath;
-                if (file.contents) {
-                    this.push(file);
-                }
-                cb();
-            }))
-            .pipe(through.obj(function (file, enc, cb) {
-                /*
-                console.log(file);
-                console.log(file.base);
-                console.log(file.path);
-                console.log(file.contents);*/
+function rewrite(revMap) {
+    return through.obj(function (obj, enc, cb) {
+        obj.contents = new Buffer(rewriter(revMap, obj.contents.toString(
+            'utf-8')));
+        this.push(obj);
+        cb();
+    });
+}
+
+function src(glob, opts) {
+    var xopts = {
+        cwd: appRoot
+    };
+    opts = opts ? assign({}, xopts, opts) : xopts;
+    return gulp.src.call(gulp, glob, opts);
+}
+
+function dest() {
+    var destPath = path.join.apply(path, [appRoot].concat([].slice.call(
+        arguments)));
+    return gulp.dest(destPath);
+}
+
+function tRev(prefix) {
+    return streamCombine(
+        through.obj(function (obj, enc, cb) {
+            obj.base = prefix ? path.join(appRoot, prefix) : appRoot;
+            this.push(obj);
+            cb();
+        }),
+        $.rev()
+    );
+}
+
+function tDest(type) {
+    return streamCombine(
+        dest('dist'), // write revisioned assets to /dist
+        through.obj(function (obj, enc, cb) {
+            console.log(obj.path);
+            this.push(obj);
+            cb();
+        }),
+        $.rev.manifest(), // generate a revision manifest file
+        $.rename(type + '-rev.json'), // generate a revision manifest file
+        through.obj(function (obj, enc, cb) {
+            console.log(obj.path);
+            this.push(obj);
+            cb();
+        }),
+        dest('dist') // write it to /dist/rev-manifest.json
+    );
+}
+
+gulp.task('build-assets', function () {
+    return src(['./assets/img/**/*.*', './ccc/*/img/**/*.*'])
+        .pipe(tRev())
+        .pipe(tDest('assets'));
+});
+
+function sCss() {
+    return es.merge(
+        src(['./assets/css/**/*.css', './ccc/*/css/**/*.css']),
+        src(['./assets/css/**/*.less', './ccc/*/css/**/*.less'], {
+            read: false
+        })
+        .pipe(through.obj(function (file, enc, done) {
+            dsAssets.renderLess(file.path, {
+                componentsDirName: 'ccc',
+                appRoot: appRoot
+            }, function (css) {
+                file.contents = new Buffer(css);
+                file.path = file.path.replace(/\.less$/,
+                    '.css');
                 this.push(file);
-                var relativePath = path.relative(rootPath, file.path);
-                var index = relativePath.indexOf('/assets');
-                var relativeDir = relativePath.substring(0, index);
-                var reqPath = relativePath.substring(index);
-                var extname = path.extname(reqPath);
-                var revvedPath = reqPath
-                    .substring(0, reqPath.length - extname.length) +
-                    '@' + relativeDir + extname;
-                assetsCache[revvedPath] = {
-                    path: reqPath,
-                    revvedPath: revvedPath,
-                    mime: mime.lookup(extname),
-                    contents: file.contents
-                };
-                assetsRevMap[reqPath] = revvedPath;
-                console.log(assetsCache);
-                console.log(assetsRevMap);
-                cb();
-            }));
+                done();
+            }.bind(this));
+        })));
+}
+
+gulp.task('build-css', ['build-assets'], function () {
+    return es.merge(
+        sCss(),
+        sCss()
+        .pipe($.mqRemove({
+            width: '1024px'
+        }))
+        .pipe($.rename({
+            suffix: '.nmq',
+            extname: '.css'
+        })))
+        .pipe(rewrite(JSON.parse(fs.readFileSync(path.join(appRoot,
+            'dist', 'assets-rev.json'), 'utf-8')), '/assets/'))
+        .pipe(tRev())
+        .pipe(tDest('css'));
+});
+
+gulp.task('build-js', ['build-css'], function () {
+    var bcp = fs.readFileSync(require.resolve(
+        'browserify-common-prelude/dist/bcp.min.js'), 'utf-8');
+    return es.merge(src(['./node_modules/assets/js/main/**/*.js',
+            './node_modules/ccc/*/js/main/**/*.js'
+        ], {
+            base: appRoot
+        })
+        .pipe($.factorBundle({
+            alterPipeline: function alterPipeline(pipeline, b) {
+                pipeline.get('pack')
+                    .splice(0, 1, bpack(assign({}, b._options, {
+                        raw: true,
+                        hasExports: false,
+                        prelude: bcp
+                    })));
+            },
+            basedir: appRoot,
+            commonJsPath: 'node_modules/assets/js/common/global.js' //"node_modules" will be removed
+        })), src(['node_modules/assets/js/**/*.js', '!**/js/main/**']))
+        .pipe(rewrite(JSON.parse(fs.readFileSync(path.join(appRoot,
+            'dist', 'assets-rev.json'), 'utf-8'))))
+        .pipe(rewrite(JSON.parse(fs.readFileSync(path.join(appRoot,
+            'dist', 'css-rev.json'), 'utf-8'))))
+        .pipe(tRev('node_modules'))
+        .pipe($.uglify({
+            compress: {
+                drop_console: true
+            },
+            output: {
+                ascii_only: true,
+                quote_keys: true
+            }
+        }))
+        .pipe(tDest('js'));
+});
+
+function getAllRevManifest() {
+    return assign({},
+        JSON.parse(fs.readFileSync(path.join(appRoot, 'dist',
+            'assets-rev.json'), 'utf-8')),
+        JSON.parse(fs.readFileSync(path.join(appRoot, 'dist',
+                'css-rev.json'),
+            'utf-8')),
+        JSON.parse(fs.readFileSync(path.join(appRoot, 'dist',
+                'js-rev.json'),
+            'utf-8'))
+    );
+}
+
+gulp.task('build', ['build-js'], function () {
+    fs.writeFileSync(path.join(appRoot, 'dist', 'rev.json'),
+        JSON.stringify(getAllRevManifest(), null, '    '), 'utf-8');
+});
+
+gulp.task('build-and-clean', ['build'], function () {
+    return src('./dist/**/*')
+        .pipe($.revOutdated(3))
+        .pipe($.rimraf());
+});
+
+/* isMasterRunning
+ * 判断 pm master 是否正在运行
+ * 在运行，返回 pid(string)
+ * 不在运行，返回 false
+ */
+function isMasterRunning() {
+    var pidFilePath = path.join(appRoot, 'ds.pid');
+    var strpid;
+    var isRunning;
+    if (fs.existsSync(pidFilePath)) {
+        strpid = fs.readFileSync(pidFilePath, 'utf-8');
+        isRunning = require('is-running')(parseInt(strpid, 10));
     }
-
-    gulp.task('assets', function () {
-        return processAssets(src([
-            './components/**/assets/**',
-            '!./components/**/assets/**/*.less'
-        ]));
-    });
-
-    gulp.task('watch:assets', ['assets'], function () {
-        return processAssets($.watch([
-            './components/**/assets/**',
-            '!./components/**/assets/**/*.less'
-        ]));
-    });
-
-    var cssRootPath = path.join(rootPath, 'assets', 'css');
-    var lessFiles = [];
-
-    function buildComponentsLess() {
-        var content = lessFiles
-            .map(function (relativePath) {
-                return '@import "' + relativePath +
-                    '";';
-            })
-            .join('\n');
-        var filePath = path.join(rootPath, 'assets', 'css',
-            'components.less');
-        assets.renderLess(filePath, content, function (css) {
-            console.log(css);
-            var revvedCss = rewrite({
-                revMap: assetsRevMap
-            }, css);
-            console.log(revvedCss);
-            assetsCache['/assets/css/components.css'] = {
-                path: '/assets/css/components.css',
-                revvedPath: '/assets/css/components.css',
-                mime: 'text/css; charset=utf-8',
-                contents: revvedCss
-            };
-            console.log(assetsCache);
-            console.log(assetsRevMap);
-        });
-        assetsRevMap['/assets/css/components.css'] =
-            '/assets/css/components.css';
+    if (isRunning) {
+        return strpid;
     }
-    //            .pipe($.watch('./components/**/assets/css/style.less'))
+    return false;
+}
 
-    gulp.task('less', function () {
-        return src('./components/**/assets/css/style.less')
-            .pipe(through.obj(function (file, enc, cb) {
-                var relativePath = path.relative(cssRootPath, file.path);
-                if (lessFiles.indexOf(relativePath) === -1) {
-                    lessFiles.push(relativePath);
-                    this.push(file);
-                }
-                cb();
-            }))
-            .on('end', buildComponentsLess);
-    });
-    gulp.task('watch:less', function () {
-        return $.watch('./components/**/assets/css/style.less')
-            .pipe(through.obj(function (file, enc, cb) {
-                this.push(file);
-                buildComponentsLess();
-                cb();
-            }));
-    });
+function reload() {
+    var strpid = isMasterRunning();
+    if (strpid) {
+        console.log('pm master is running. sending USR1 signal...');
+        exec('kill -s USR1 ' + strpid);
+    } else {
+        console.log('pm master is not running. spawning one...');
+        spawn(process.execPath, [path.join(appRoot, 'master.js')], {
+            cwd: appRoot,
+            env: assign({}, process.env, {
+                NODE_ENV: 'production'
+            }),
+            silent: true,
+            detached: true,
+            stdio: ['ignore', 'ignore', 'ignore']
+        })
+            .unref();
+    }
+}
 
-    gulp.task('dev', ['assets', 'less'], function () {
-        assets
-            .argmentApp(app, {
-                assetsCache: assetsCache
-            });
-        var server = require('http')
-            .createServer(app);
-        server.listen(function () {
-            console.log(
-                '----------\ndev server listening http://localhost:' +
-                server.address()
-                .port + '\n----------');
-        });
-        gulp.run('watch:assets', 'watch:less');
+gulp.task('start', ['build-and-clean'], reload);
+gulp.task('reload', reload);
+gulp
+    .task('stop', function () {
+        var strpid = isMasterRunning();
+        if (strpid) {
+            console.log('pm master is running. terminating...');
+            exec('kill -s TERM ' + strpid);
+        } else {
+            console.log('pm master is not running. exiting...');
+        }
     });
 
-    gulp.task('default', ['dev']);
+gulp.task('dev', function () {
+    var watchifyMiddleware = watchify.middleware();
+    var watchifyApp = require('express')();
+    watchifyApp.set('etag', false);
+    watchifyApp.use(require('morgan')());
+    watchifyApp.use(watchifyMiddleware);
+    watchifyApp.listen(port + 1000, function () {
+        console.log("watchify listening at http://127.0.0.1:%d",
+            this.address()
+            .port);
+    });
+
+    $.supervisor(path.join(appRoot, 'index.js'), {
+        ext: ['json', 'js'],
+        args: ['--run-by-gulp'],
+        ignore: ['node_modules', 'assets']
+    });
+});
+    
 };
